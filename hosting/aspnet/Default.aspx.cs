abf12408495d986760dfb673bbd77e1abd918948
Jimmy Schementi <jschementi@gmail.com>
2009-02-03 17:48:15 -0800

    Simple DLR Hosting examples for WPF and ASP.NET

---
+++ hosting/aspnet/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Scripting.Hosting;
+
+namespace aspnet
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var runtime = ScriptRuntime.CreateFromConfiguration();
+            var engine = runtime.GetEngine("IronRuby");
+            var scope = engine.CreateScope();
+            scope.SetVariable("page", this);
+            engine.Execute("page.Message.Text = 'Hello from Ruby!'", scope);
+        }
+    }
+}
---
+++ hosting/wpf/Window1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Scripting.Hosting;
+
+
+namespace wpf
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            var runtime = ScriptRuntime.CreateFromConfiguration();
+            var engine = runtime.GetEngine("IronRuby");
+            var scope = engine.CreateScope();
+            scope.SetVariable("window", this);
+            engine.Execute("window.Message.Text = 'Hello from Ruby!'", scope);
+        }
+    }
+}
