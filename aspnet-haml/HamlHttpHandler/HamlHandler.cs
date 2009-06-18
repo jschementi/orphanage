90b17c9efaec01428c9b9dc8d7f06ec0a0b3dfbe
Jimmy Schementi <jschementi@gmail.com>
2009-06-18 12:34:09 -0700

    Haml / Sass pages on IIS

---
+++ aspnet-haml/HamlHttpHandler/HamlHandler.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.IO;
+using IronRuby.Builtins;
+
+namespace IronRuby.Haml
+{
+    public class HamlHandlerFactory : IHttpHandlerFactory
+    {
+        private static HamlHandler _Handler;
+
+        public IHttpHandler GetHandler(HttpContext/*!*/ context, string/*!*/ requestType, string/*!*/ url, string/*!*/ pathTranslated)
+        {
+            if (_Handler == null)
+            {
+                try
+                {
+                    _Handler = new HamlHandler();
+                }
+                catch (Exception e)
+                {
+                    context.Response.StatusCode = 200;
+                    return null;
+                }
+            }
+
+            return _Handler;
+        }
+
+        public void ReleaseHandler(IHttpHandler/*!*/ handler)
+        {
+
+        }
+    }
+
+    internal sealed class HamlHandler : IHttpHandler
+    {
+        public HamlHandler()
+        {
+            RubyEngine.Init();
+            RubyEngine.Require("rubygems");
+            RubyEngine.Require("haml");
+            RubyEngine.Require("sass");
+        }
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string path = context.Request.PhysicalPath;
+            string directory = Path.GetDirectoryName(path);
+            var extension = Path.GetExtension(path);
+
+            if (extension == string.Empty) {
+                if (File.Exists(directory + Path.DirectorySeparatorChar + "index.haml"))
+                {
+                    path = directory + Path.DirectorySeparatorChar + "index.html";
+                    extension = ".html";
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            if (File.Exists(path))
+            {
+                context.Response.Write(File.ReadAllText(path));
+                return;
+            }
+
+            string type = null;
+            if (extension == ".html")
+            {
+                extension = ".haml";
+                type = "Haml";
+            }
+            else if (extension == ".css")
+            {
+                extension = ".sass";
+                type = "Sass";
+            }
+
+            string realPath = directory + Path.DirectorySeparatorChar.ToString() + 
+                Path.GetFileNameWithoutExtension(path) + extension;
+            if (!File.Exists(realPath))
+            {
+                context.Response.StatusCode = 404;
+                return;
+            }
+
+            string template = File.ReadAllText(realPath);
+            context.Response.Write(RenderTemplate(template, type));
+        }
+
+        private string RenderTemplate(string template, string engine) 
+        {    
+            var constant = RubyEngine.Execute(engine + "::Engine");
+            var engineObject = RubyEngine.ExecuteMethod<RubyObject>(constant, "new", MutableString.Create(template));
+            return RubyEngine.ExecuteMethod<MutableString>(engineObject, "render").ToString();
+        }
+    }
+}
---
+++ aspnet-haml/HamlHttpHandler/RubyEngine.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Microsoft.Scripting.Hosting;
+using IronRuby.Builtins;
+using IronRuby.Runtime;
+using System.Configuration;
+
+namespace IronRuby.Haml {
+    public static class RubyEngine {
+        public static ScriptEngine Engine;
+        private static ScriptScope scope;
+
+        public static void Init() {
+            Engine = Ruby.CreateEngine();
+            scope = Engine.CreateScope();
+        }
+
+        public static RubyContext Context {
+            get {
+                return Ruby.GetExecutionContext(Engine);
+            }
+        }
+
+        public static object Require(string file) {
+            return Require(file, null);
+        }
+
+        public static object Require(string file, string gemVersion) {
+            var command = new MutableString();
+            if(gemVersion != null) {
+                command.Append(string.Format("gem '{0}', '{1}';", file, gemVersion));
+            }
+            command.Append(string.Format("require '{0}'", file));
+            return Execute(command);
+        }
+
+        public static object ExecuteFile(string fileName) {
+            return Engine.CreateScriptSourceFromString(FindFile(fileName)).Execute(scope);
+        }
+
+        public static object Execute(string code) {
+            return Execute(code, scope);
+        }
+
+        public static object Execute(string code, ScriptScope aScope) {
+            return Engine.CreateScriptSourceFromString(code).Execute(aScope);
+        }
+
+        public static T ExecuteMethod<T>(object instance, string methodName)
+        {
+            return ExecuteMethod<T>(instance, methodName, new object[] { });
+        }
+
+        public static T ExecuteMethod<T>(object instance, string methodName, params object[] args)
+        {
+            return (T)Engine.Operations.InvokeMember(instance, methodName, args);
+        }
+
+        public static object AddLoadPath(string path) {
+            return Engine.Execute(string.Format("$LOAD_PATH.unshift '{0}'", path));
+        }
+
+        public static void SetConstant(string name, object value) {
+            Engine.Runtime.Globals.SetVariable(name, value);
+        }
+
+        public static string FindFile(string file) {
+            foreach (var path in Engine.GetSearchPaths()) {
+                var fullPath = TryGetFullPath(path, file);
+                if (File.Exists(fullPath)) {
+                    return fullPath;
+                }
+            }
+            return null;
+        }
+
+        public static void SetGemPath()
+        {
+            string gempath = ConfigurationManager.AppSettings["GEM_PATH"];
+            if (gempath != null)
+            {
+                gempath = Path.GetFullPath(gempath);
+
+                if (Directory.Exists(gempath))
+                {
+                    Environment.SetEnvironmentVariable("GEM_PATH", gempath);
+                }
+            }
+        }
+
+        private static string TryGetFullPath(string/*!*/ dir, string/*!*/ file) {
+            try {
+                return Path.GetFullPath(Path.Combine(dir, file));
+            } catch {
+                return null;
+            }
+        }
+    
+    }
+}
