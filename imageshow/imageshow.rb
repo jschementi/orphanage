b4037f9acb06c67569eb9a19b4526ebc3e7cbbdc
Jimmy Schementi <jschementi@gmail.com>
2010-01-20 21:01:15 -0800

    Make imagesizer work on the latest version of IronRuby

--- imageshow/imageshow.rb
+++ imageshow/imageshow.rb
@@ -27,29 +27,37 @@ class ImageShow
     w.size_to_content = SizeToContent.width_and_height
     w.background = black
     w.window_style = WindowStyle.tool_window
-    w.resize_mode = ResizeMode.no_resize
+    w.resize_mode = ResizeMode.NoResize
     w.title = "IronRuby Image Shower"
     
     grid = Grid.new
     grid.background = gray
 
-    add_stack_panel(grid, :vertical, Thickness.new(20)) do |sp|
+    width = 10
+    size = [ 
+      if @urls.size == 4 then width / 2
+      elsif @urls.size == 2 then [width / 2, width]
+      else width 
+      end 
+    ].flatten.compact
+
+    add_stack_panel(grid, :vertical, Thickness.new(1)) do |sp|
       add_stack_panel(sp, :horizontal) do |sph|
-        add_stack_panel(sph, :vertical, Thickness.new(20, 20, 10, 10)) do |spv|
+        add_stack_panel(sph, :vertical, Thickness.new(width, width, size[0], size[-1])) do |spv|
           add_image(@urls[0], spv)
         end
         if @urls.size > 1
-          add_stack_panel(sph, :vertical, Thickness.new(10, 20, 20, 10)) do |spv|
+          add_stack_panel(sph, :vertical, Thickness.new(size[0], width, width, size[-1])) do |spv|
             add_image(@urls[1], spv)
           end
         end
       end
       if @urls.size == 4
         add_stack_panel(sp, :horizontal) do |sph|
-          add_stack_panel(sph, :vertical, Thickness.new(20, 10, 10, 20)) do |spv|
+          add_stack_panel(sph, :vertical, Thickness.new(width, size[0], size[-1], width)) do |spv|
             add_image(@urls[2], spv)
           end
-          add_stack_panel(sph, :vertical, Thickness.new(10, 10, 20, 20)) do |spv|
+          add_stack_panel(sph, :vertical, Thickness.new(size[0], size[-1], width, width)) do |spv|
             add_image(@urls[3], spv)
           end
         end
